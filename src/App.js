3b135fec3cf4203bf24a43eb64bbed4b23d3eccf
Nisha Devi <nishadevi0210@gmail.com>
2021-07-16 12:28:55 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router} from "react-router-dom";
 
 
 function App() {
   return (
-    <div className="App">
-      hi
+    <Router>
+      <div className="App">
+        hi
     </div>
+    </Router>
+    
   );
 }
 
